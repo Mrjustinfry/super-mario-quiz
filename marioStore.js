9def9aae85e89685bb1e51afbe343f38cb0953d3
Justin Fry <mrjustinfry@gmail.com>
2018-07-30 12:26:48 -0400

    initial commit

---
+++ marioStore.js
@@ -0,0 +1,100 @@
+//question answer array
+const STORE = [
+  {//question[0]
+    question: `What is Mario's brother's name?`,
+    answerChoice: [`Bowser`, `Toad`,`Luigi`,`Gino`],
+    correctAnswer: `Luigi` //answerChoice[2]
+  },
+  {//question[1]
+    question: `What was Mario called before he was named Mario?`,
+    answerChoice: [`Plumber-guy`, `Jumpman`, `Charlie`,`NintenDude`],
+    correctAnswer: `Jumpman` //answerChoice[1]
+  },
+  {//question[2]
+    question: `Which of these damsels was not rescued by Mario`,
+    answerChoice: [`Vivian`,`Peach`,`Paulina`, `Daisy`],
+    correctAnswer: `Vivian` //answerChoice[0]
+  },
+  {//question[3]
+    question: `Who created the Super Mario series?`,
+    answerChoice: [`Masahiro Sakurai`,`Reggie Fils-Aim\xE9`,`Charles Martinet`,`Shigeru Miyamoto`],
+    correctAnswer: `Shigeru Miyamoto` //answerChoice[3]
+  },
+  {//question[4]
+    question: `Where does Princess Peach live?`,
+    answerChoice: [`The Mushroom Kingdom`,`Chocolate Island`,`Isle Delfino`,`New Donk City`],
+    correctAnswer: `The Mushroom Kingdom` //answerChoice[0]
+  },
+  {//question[5]
+    question: `Mario and Luigi's last name is...?`,
+    answerChoice: [`Sanchez`,`Mario`,`Johnson`,`Capone`],
+    correctAnswer: `Mario` //answerChoice[1]
+  },
+  {//question[6]
+    question: `Which of the following is NOT a Koopaling?`,
+    answerChoice: [`Ludwig`,`Wendy`,`Clarence`,`Iggy`],
+    correctAnswer: `Clarence` //answerChoice[2]
+  },
+  {//question[7]
+    question: `The American version of Super Mario Bros. 2 is an altered version of what game?`,
+    answerChoice: [`The Lost Levels`,`Clu Clu Land`,`Doki Doki Panic`,`Kid Chameleon`],
+    correctAnswer: `Doki Doki Panic` //answerChoice[2]
+  },
+  {//question[8]
+    question: `Rosalina is the protector of what?`,
+    answerChoice: [`Lumas`,`Goombas`,`Bowser Jr.`,`Lakitu`],
+    correctAnswer: `Lumas` //answerChoice[0]
+  },
+  { //question[9]
+    question: `Which of the following is NOT a Super Mario spin-off?`,
+    answerChoice: [`Mario Tennis`,`Hotel Mario`,`Mario Paint`,`Chef Mario`],
+    correctAnswer: `Chef Mario` //answerChoice[3]
+  }
+] 
+
+//easter egg img array
+const easterEggs = [
+  {
+  src: `<img src="https://vignette.wikia.nocookie.net/fantendo/images/4/4c/Mushroom_%28Mario_Kart_8%29.png/revision/latest?cb=20141203012138" alt="mushroom" class="sml easterEgg" />`
+  },
+  {
+    src: `<img src="https://vignette.wikia.nocookie.net/mario/images/4/40/SuperLeafSM3DL.png/revision/latest?cb=20180110005248" alt="tanooki" class="sml easterEgg" />`
+  },
+  {
+    src: `<img src="https://vignette.wikia.nocookie.net/fantendo/images/1/1a/Propeller_Mushroom.png/revision/latest?cb=20120306105004" alt="heli-mushroom" class="sml easterEgg" />`
+  },
+  {
+    src: `<img src="http://images4.wikia.nocookie.net/__cb20121006232143/fantendo/images/5/53/PenguinSuit.png" alt="penguin" class="sml easterEgg" />`
+  },
+    {
+  src: `<img src="https://vignette.wikia.nocookie.net/fantendo/images/6/64/Boomerangflower.png/revision/latest?cb=20111020172916" alt="boomerang" class="sml easterEgg" />`
+  },
+  {
+    src: `<img src="http://img1.wikia.nocookie.net/__cb20111109223905/fantendo/images/7/70/FireFlowerSM3DL.png" alt="fireflower" class="sml easterEgg" />`
+  },
+  {
+    src: `<img src="https://vignette.wikia.nocookie.net/fantendo/images/1/13/BlueShell.png/revision/latest?cb=20110713213049" alt="blue shell" class="sml easterEgg" />`
+  },
+  {
+    src: `<img src="https://www.mariowiki.com/images/thumb/a/a7/Powerup-ice-flower-sm.png/200px-Powerup-ice-flower-sm.png" alt="ice flower" class="sml easterEgg" />`
+  },
+  {
+  src: `<img src="https://vignette.wikia.nocookie.net/mario/images/6/65/180px-WiiU_NewMarioU_4_item01_E3.png/revision/latest?cb=20141014164700&path-prefix=de" alt="acorn" class="sml easterEgg" />`
+  },
+  {
+    src: `<img src="https://vignette.wikia.nocookie.net/mariokart/images/8/8a/N64_Spiny_Shell_-_Mario_Kart_DS.png/revision/latest?cb=20171107013346" alt="spikyshell" class="sml easterEgg" />`
+  },
+  {
+    src: `<img src="https://vignette.wikia.nocookie.net/mario/images/3/3a/Mega_Mushroom_Artwork_-_New_Super_Mario_Bros.png/revision/latest?cb=20170814163102" alt="giant mushroom" class="sml easterEgg" />`
+  },
+  {
+    src: `<img src="http://files.softicons.com/download/game-icons/super-mario-icons-by-sandro-pereira/png/256/Mushroom%20-%20Spring.png" alt="metal mushroom" class="sml easterEgg" />`
+  },
+  {
+    src: `<img src="https://static.memrise.com/uploads/things/images/58777347_150429_2112_31.jpg" alt="bumble bee" class="sml easterEgg" />`
+  },
+  {
+    src: `<img src="http://oyster.ignimgs.com/mediawiki/apis.ign.com/super-mario-3d-land/6/65/201px-Star.PNG" alt="star" class="sml easterEgg" />`
+  }
+]
+
---
+++ marioquiz.js
@@ -0,0 +1,221 @@
+'use strict';
+
+let questionNum = 0;
+let scoreTotal = 0;
+
+function createQuestion() {
+  //generates question by looping through STORE array
+  for(STORE.question=0;STORE.question<=STORE.length;STORE.question++) {
+  if (questionNum < STORE.length) {
+  return `
+  <div class="row">
+      <section class="col-12 questionForm" role="region">
+      <form role="form">
+      <fieldset class="col-12" role="radiogroup">
+        <legend class="qHead">${STORE[questionNum].question}</legend>
+            <label class="answer">
+              <input type="radio" role="radio" value="${STORE[questionNum].answerChoice[0]}" class="answerOption" name="answer" required>
+                <span class="aChoice">${STORE[questionNum].answerChoice[0]}</span>
+              </label>
+              <label class="answer">
+              <input type="radio" role="radio" value="${STORE[questionNum].answerChoice[1]}" class="answerOption"  name="answer" required>
+                <span class="aChoice">${STORE[questionNum].answerChoice[1]}</span>
+              </label>
+              <label class="answer">
+              <input type="radio" role="radio" value="${STORE[questionNum].answerChoice[2]}" class="answerOption"  name="answer" required>
+                  <span class="aChoice">${STORE[questionNum].answerChoice[2]}</span>
+              </label>
+              <label class="answer">
+              <input type="radio" role="radio" value="${STORE[questionNum].answerChoice[3]}" class="answerOption"  name="answer" required>
+                <span class="aChoice">${STORE[questionNum].answerChoice[3]}</span>
+              </label>
+              <br>
+              <button class="answerBtn" role="button" value="Submit">Submit</button>
+            </fieldset>
+          </form>
+        </section>
+    </div>
+    `;
+        $(`.scoreContainer`).css('display','block');
+  } else {
+    finalScore();
+  } 
+}};
+
+//removes start page and displayed questionBox 
+function startQuiz() {
+  $('.box').on('click', '.startBtn', function (e) {
+    e.preventDefault();
+    $('.box').css('display','none');
+    $('.questionBox').css('display','block');
+    $('.questionNumber').text(1);
+  })};
+
+//renders question in the DOM
+function displayQuestion() {
+  $('.questionBox').html(createQuestion());
+}
+
+//next button functionality
+function nextQuestion() {
+  $('main').on('click', '.nextBtn', function (e) {
+    e.stopPropagation();
+    displayQuestion();
+    answerSubmit();
+    $('.questionNumber').text(questionNum+1);
+})};
+
+
+//displays correct answer feeback
+function answerCorrect() {
+  let correctAnswer = `${STORE[questionNum].correctAnswer}`;
+  $('.questionBox').html(`<div class="row">
+      <div class="col-12">
+        <section class="answerBoxCorrect" role="region">
+        <h1 class="response">Wahoo!</h1>
+        <img src="http://www.stickpng.com/assets/images/584df3956a5ae41a83ddee07.png" alt="Mario" class="med" />
+        <p class="fBack"><span>${STORE[questionNum].correctAnswer}</span> is correct!</p>
+        <button class="nextBtn correctButton" role="button">Continue</button>
+        </section>
+        </div>
+      </div>`);
+      scoreBoard();
+}
+
+//displays incorrect answer feedback
+function answerWrong() {
+    let correctAnswer = `${STORE[questionNum].correctAnswer}`;
+    let choice = $('input:checked');
+  $('.questionBox').html(`<div class="row">
+      <div class="col-12">
+        <section class="answerBoxWrong" role="region">
+        <h1 class="response">Oh No!</h1>
+        <img src="https://cdn-images-1.medium.com/max/1600/1*x2Gw1gKrRQi5WEcBrKYA1g.png" alt="Bowser" class="med" />
+        <p class="fBack"><span>${choice.val()}</span> is incorrect!<br>The correct answer is: <span class="corAns">${STORE[questionNum].correctAnswer}</span></p>
+        <button class="nextBtn wrongButton" role="button">Continue</button>
+        </section>
+        </div>
+      </div>`);
+}
+
+//increments score
+function scoreBoard() {
+  $(`.scoreTotal`).text(scoreTotal+1);
+  scoreTotal++;
+}
+
+//increments question number
+function questionTally() {
+  questionNum++;
+}
+
+//validates answer submission and calls appropriate feedback
+function answerSubmit() {
+  $(`.questionForm`).on('submit', function (e) {
+    e.preventDefault();
+    let choice = $('input:checked');
+    let userAnswer = choice.val();
+    let correctAnswer = `${STORE[questionNum].correctAnswer}`; 
+    if (userAnswer === correctAnswer) {
+      answerCorrect();
+    } else {
+      answerWrong();
+    }
+    questionTally();
+  });
+}
+
+//determines final page based on score
+function finalScore() {
+if (scoreTotal >= 8){
+  $(`.questionBox`).html(resultPageThree());
+} else if (scoreTotal > 3 && scoreTotal < 8){
+  $(`.questionBox`).html(resultPageTwo());  
+} else if (scoreTotal <= 3) {
+    $(`.questionBox`).html(resultPageOne());
+}
+  $(`.scoreContainer`).css('display','none');
+}
+
+function restartQuiz() {
+   $('main').on('click', '.restartBtn', function (e) {
+    questionNum = 0;
+    scoreTotal = 0;
+    e.stopPropagation();
+      $(`.scoreContainer`).css('display','block');
+      $('.questionNumber').text(0);
+      $(`.scoreTotal`).text(0);
+      $('.box').css('display','block');
+      $('.questionBox').css('display','none');
+      startQuiz();
+      displayQuestion();
+      answerSubmit();
+      nextQuestion();
+  });
+ console.log('`restartQuiz` ran');
+}
+
+//final page for low score
+function resultPageOne() {
+  return `<div class="row">
+        <div class="col-12 finalPageOne">
+          <section role="region">
+            <h1>Bwah Ha Ha!</h1>
+            <img src="https://img00.deviantart.net/5a51/i/2018/178/d/3/super_smash_bros_ultimate_render__15__bowser_by_falconburst322-dcfnc2z.png" alt="Toad" class="med" />            
+            <h3>Better luck next time!</h3><br>
+            <p class="bowserScore">You got ${scoreTotal} out of 10 correct!</p><br>
+            <button class="restartBtn bowserBtn" role="button">Try again?</button>
+          </section>
+        </div>
+      </div>`
+}
+
+//final page for median score
+function resultPageTwo() {
+  return `<div class="row">
+        <div class="col-12 finalPageTwo">
+          <section role="region">
+            <h1>Thank you Mario!</h1>
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d1/Toad_3D_Land.png/220px-Toad_3D_Land.png" alt="Toad" class="med" />            
+            <h3>...but our princess is in another castle.</h3><br>
+            <p class="toadScore">You got ${scoreTotal} out of 10 correct!</p><br>
+            <button class="restartBtn toadBtn" role="button">Try again?</button>
+          </section>
+        </div>
+      </div>`
+}
+
+//final page for high score
+function resultPageThree() {
+  return `<div class="row">
+        <div class="col-12 finalPageThree">
+          <section role="region">
+            <h1><img src="https://cdn.shopify.com/s/files/1/1061/1924/products/Sparkling_Pink_Heart_Emoji_large.png?v=1480481032" alt="heart" class="x-sml" />Oh Mario!<img src="https://cdn.shopify.com/s/files/1/1061/1924/products/Sparkling_Pink_Heart_Emoji_large.png?v=1480481032" alt="heart" class="x-sml" /></h1>
+            <img src="https://vignette.wikia.nocookie.net/donkeykong/images/7/74/Princess_peach.png/revision/latest?cb=20160602075029" alt="Toad" class="med" />
+            <h3>You're my hero!</h3>
+            <p class="peachScore">You got ${scoreTotal} out of 10 correct!</p><br>
+            <button class="restartBtn peachBtn" role="button">Try again?</button>
+          </section>
+        </div>
+      </div>`
+}
+
+//displays random img from easter egg array
+function easterEgg() {
+$('.easterEgg').click(function() {
+  let randomEgg = easterEggs[Math.floor(Math.random() * easterEggs.length)];
+  $('.easterEggBox').html(randomEgg.src);
+  easterEgg();
+});
+}
+
+function marioQuiz() {
+startQuiz();
+displayQuestion();
+answerSubmit();
+nextQuestion();
+restartQuiz();
+easterEgg();
+}
+
+$(marioQuiz);
